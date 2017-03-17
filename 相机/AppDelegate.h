8679d0628c6ba40d8fe5537a8950c96bf204ed41
kuangzihui <ejt_ios@ejt-iosdemac-mini.local>
2017-03-17 14:51:31 +0800

    Initial Commit

---
+++ 相机/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  相机
+//
+//  Created by ejt_ios on 17/3/17.
+//  Copyright © 2017年 ejt_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
