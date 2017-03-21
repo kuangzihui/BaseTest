c78893fab5997f3a5dae36be22174e8b50d65f16
kuangzihui <kuangzihui@163.com>
2017-03-21 16:54:24 +0800

    第一次上传

---
+++ 相机/CameraOverlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraOverlayViewController.h
+//  相机
+//
+//  Created by ejt_ios on 17/3/17.
+//  Copyright © 2017年 ejt_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraOverlayViewController : UIViewController
+
+@end
